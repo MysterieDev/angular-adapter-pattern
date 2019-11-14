857d8e264ab84776ed3a911162075d6b6169c39d
MysterieDev <josefsle97@gmail.com>
2019-11-14 10:05:08 +0100

    adding thumbnail to adapter

--- src/app/drink.model.ts
+++ src/app/drink.model.ts
@@ -1,14 +1,18 @@
 export class Drink{
 name: String;
+imgThumb: String;
 constructor (
-  name: String
+  name: String,
+  imgThumb: String
 ){
 this.name = name;
+this.imgThumb = imgThumb;
 }
 
 static adapt(item: any){
   return new Drink (
-    item.strDrink
+    item.strDrink,
+    item.strDrinkThumb
   );
 }
 
